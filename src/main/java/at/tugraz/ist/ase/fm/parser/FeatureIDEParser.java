78af58f9e3616ba4400619976b800a1dbecf169d
Lê Viết Mẫn <manleviet@gmail.com>
2022-01-22 01:09:02 +0100

    Add FeatureIDEParser

---
+++ src/main/java/at/tugraz/ist/ase/fm/parser/FeatureIDEParser.java
@@ -0,0 +1,317 @@
+/*
+ * at.tugraz.ist.ase.fm - A Maven package for feature models
+ *
+ * Copyright (c) 2022
+ *
+ * @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+ */
+
+package at.tugraz.ist.ase.fm.parser;
+
+import at.tugraz.ist.ase.common.LoggerUtils;
+import at.tugraz.ist.ase.fm.core.Feature;
+import at.tugraz.ist.ase.fm.core.FeatureModel;
+import at.tugraz.ist.ase.fm.core.FeatureModelException;
+import at.tugraz.ist.ase.fm.core.RelationshipType;
+import com.google.common.annotations.Beta;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * A parser for the FeatureIDE format
+ */
+@Beta
+@Slf4j
+public class FeatureIDEParser implements FeatureModelParser {
+
+    /**
+     * Check whether the format of the given file is FeatureIDE format
+     *
+     * @param filePath - a {@link File}
+     * @return true - if the format of the given file is FeatureIDE format
+     *          false - otherwise
+     */
+    @Override
+    public boolean checkFormat(@NonNull File filePath) {
+        // first, check the extension of file
+        checkArgument(filePath.getName().endsWith(".xml"), "The file is not in FeatureIDE format!");
+
+        // second, check the structure of file
+        try {
+            // read the file
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(filePath.toString());
+            Element rootEle = doc.getDocumentElement();
+
+            // if it has three tag "featureModel", "struct" and "constraints"
+            if (rootEle.getTagName().equals("featureModel") &&
+                    rootEle.getElementsByTagName("struct").getLength() > 0 &&
+                    rootEle.getElementsByTagName("constraints").getLength() > 0) {
+                return true;
+            }
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            return false; // if it raise an exception, it's not FeatureIDE format
+        }
+        return false;
+    }
+
+    /**
+     * This function parse the given {@link File} into a {@link FeatureModel}.
+     *
+     * @param filePath - a {@link File}
+     * @return a {@link FeatureModel}
+     * @throws FeatureModelParserException when error occurs in parsing
+     */
+    @Override
+    public FeatureModel parse(@NonNull File filePath) throws FeatureModelParserException {
+        checkArgument(checkFormat(filePath), "The format of file is not FeatureIDE format or there are errors in the file!");
+
+        log.trace("{}Parsing the feature model file [file={}] >>>", LoggerUtils.tab, filePath.getName());
+        LoggerUtils.indent();
+
+        FeatureModel featureModel;
+        try {
+            // read XML file
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(filePath.toString());
+            doc.getDocumentElement().normalize();
+            Element rootEle = doc.getDocumentElement();
+
+            checkState(rootEle != null, "DocumentBuilder couldn't parse the document! There are errors in the file.");
+
+            // create the feature model
+            featureModel = new FeatureModel();
+
+            convertStructNodes(rootEle, featureModel);
+
+            if (featureModel.getNumOfFeatures() == 0) {
+                throw new FeatureModelParserException("Couldn't parse any features in the feature model file!");
+            }
+
+            convertConstraintNodes(rootEle, featureModel);
+        } catch (SAXException | IOException | ParserConfigurationException ex) {
+            throw new FeatureModelParserException(ex.getMessage());
+        }
+
+        LoggerUtils.outdent();
+        log.debug("{}<<< Parsed feature model [file={}, fm={}]", LoggerUtils.tab, filePath.getName(), featureModel);
+        return featureModel;
+    }
+
+    /**
+     * Take the "struct" node and convert its child nodes into features
+     * and relationships in the {@link FeatureModel}.
+     *
+     * @param rootEle - a XML root element
+     * @param fm - a {@link FeatureModel}
+     */
+    private void convertStructNodes(Element rootEle, FeatureModel fm) throws FeatureModelParserException {
+        log.trace("{}Generating features and relationships >>>", LoggerUtils.tab);
+        LoggerUtils.indent();
+
+        NodeList struct = rootEle.getElementsByTagName("struct");
+
+        examineAStructNode(struct.item(0), fm);
+
+        LoggerUtils.outdent();
+    }
+
+    /**
+     * Examine a XML node to convert child nodes into features, and relationships
+     * of a {@link FeatureModel}.
+     *
+     * @param node - a XML node
+     * @param fm - a {@link FeatureModel}
+     * @throws FeatureModelParserException when error occurs in parsing
+     */
+    private void examineAStructNode(Node node, FeatureModel fm) throws FeatureModelParserException {
+        try {
+            NodeList children = node.getChildNodes();
+            Element parentElement = (Element) node;
+            // take children names
+            List<String> childrenName = getChildrenName(node);
+            for (String childName : childrenName) {
+                fm.addFeature(childName, childName);
+            }
+
+            // convert relationships
+            if (!node.getNodeName().equals("struct")) {
+                // relationships
+                Feature leftSide;
+                List<Feature> rightSide;
+                RelationshipType type;
+
+                switch (node.getNodeName()) {
+                    case "and":
+                        for (int i = 0; i < children.getLength(); i++) {
+                            Node child = children.item(i);
+
+                            if (isCorrectNode(child)) {
+                                Element childElement = (Element) child;
+
+                                if (childElement.getAttribute("mandatory").equals("true")) {
+                                    // MANDATORY
+                                    leftSide = fm.getFeature(parentElement.getAttribute("name"));
+                                    rightSide = new LinkedList<>();
+                                    rightSide.add(fm.getFeature(childElement.getAttribute("name")));
+                                    type = RelationshipType.MANDATORY;
+                                } else {
+                                    // OPTIONAL
+                                    leftSide = fm.getFeature(childElement.getAttribute("name"));
+                                    rightSide = new LinkedList<>();
+                                    rightSide.add(fm.getFeature(parentElement.getAttribute("name")));
+                                    type = RelationshipType.OPTIONAL;
+                                }
+
+                                fm.addRelationship(type, leftSide, rightSide);
+                            }
+                        }
+
+                        break;
+                    case "or":
+                        leftSide = fm.getFeature(parentElement.getAttribute("name"));
+                        rightSide = new LinkedList<>();
+                        for (String childName : childrenName) {
+                            rightSide.add(fm.getFeature(childName));
+                        }
+                        type = RelationshipType.OR;
+
+                        fm.addRelationship(type, leftSide, rightSide);
+                        break;
+                    case "alt":
+                        leftSide = fm.getFeature(parentElement.getAttribute("name"));
+                        rightSide = new LinkedList<>();
+                        for (String childName : childrenName) {
+                            rightSide.add(fm.getFeature(childName));
+                        }
+                        type = RelationshipType.ALTERNATIVE;
+
+                        fm.addRelationship(type, leftSide, rightSide);
+                        break;
+                }
+            }
+
+            // examine sub-nodes
+            for (int i = 0; i < children.getLength(); i++) {
+                Node child = children.item(i);
+                if (isCorrectNode(child)) {
+                    examineAStructNode(child, fm);
+                }
+            }
+        } catch (FeatureModelException e) {
+            throw new FeatureModelParserException("Couldn't parse the node [" + node + "]");
+        }
+    }
+
+    /**
+     * Take the names of child nodes of a XML node.
+     *
+     * @param node - a XML node
+     * @return an array of names of child nodes of a given XML node
+     */
+    private List<String> getChildrenName(Node node) {
+        NodeList children = node.getChildNodes();
+        List<String> names = new LinkedList<>();
+
+        for (int i = 0; i < children.getLength(); i++)
+        {
+            Node child = children.item(i);
+            if (isCorrectNode(child)) {
+                Element childElement = (Element) child;
+
+                names.add(childElement.getAttribute("name"));
+            }
+        }
+
+        return names;
+    }
+
+    /**
+     * Check whether a {@link Node} is a Element node
+     * and the node name is "and" or "or" or "alt" or "feature"
+     *
+     * @param node - a {@link Node}
+     * @return true if it's correct, false otherwise
+     */
+    private boolean isCorrectNode(Node node) {
+        return node.getNodeType() == Node.ELEMENT_NODE
+                && (node.getNodeName().equals("and")
+                || node.getNodeName().equals("or")
+                || node.getNodeName().equals("alt")
+                || node.getNodeName().equals("feature"));
+    }
+
+    /**
+     * Take "rule" nodes and convert them into constraints in {@link FeatureModel}.
+     *
+     * @param rootEle - the root element
+     * @param fm - a {@link FeatureModel}
+     */
+    private void convertConstraintNodes(Element rootEle, FeatureModel fm) throws FeatureModelParserException {
+        log.trace("{}Generating constraints >>>", LoggerUtils.tab);
+        LoggerUtils.indent();
+
+        NodeList rules = rootEle.getElementsByTagName("rule");
+
+        for (int i = 0; i < rules.getLength(); i++) {
+            examineARuleNode(rules.item(i), fm);
+        }
+
+        LoggerUtils.outdent();
+    }
+
+    /**
+     * Examine a "rule" node to convert into a constraint
+     *
+     * @param node - an XML node
+     * @param fm - a {@link FeatureModel}
+     * @throws FeatureModelParserException - if the node is not a "rule" node
+     */
+    private void examineARuleNode(Node node, FeatureModel fm) throws FeatureModelParserException {
+        try {
+            Node n = node.getChildNodes().item(1);
+
+            Feature left;
+            List<Feature> rightSideList = new LinkedList<>();
+            RelationshipType type;
+
+            switch (n.getNodeName()) {
+                case "imp" -> {
+                    left = fm.getFeature(n.getChildNodes().item(1).getTextContent());
+                    rightSideList.add(fm.getFeature(n.getChildNodes().item(3).getTextContent()));
+                    type = RelationshipType.REQUIRES;
+                }
+                case "disj" -> {
+                    NodeList n1 = n.getChildNodes();
+                    left = fm.getFeature(n1.item(1).getChildNodes().item(1).getTextContent());
+                    rightSideList.add(fm.getFeature(n1.item(3).getChildNodes().item(1).getTextContent()));
+                    type = RelationshipType.EXCLUDES;
+                }
+
+                default -> throw new FeatureModelParserException(n.getNodeName() + " is an wrong name for constraints!");
+            }
+
+            fm.addConstraint(type, left, rightSideList);
+        } catch (FeatureModelException e) {
+            throw new FeatureModelParserException("Couldn't parse the node [" + node + "]");
+        }
+    }
+}
---
+++ src/test/java/at/tugraz/ist/ase/fm/parser/FeatureIDEParserTest.java
@@ -0,0 +1,57 @@
+/*
+ * at.tugraz.ist.ase.fm - A Maven package for feature models
+ *
+ * Copyright (c) 2022
+ *
+ * @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+ */
+
+package at.tugraz.ist.ase.fm.parser;
+
+import at.tugraz.ist.ase.fm.core.FeatureModel;
+import at.tugraz.ist.ase.fm.parser.factory.FMParserFactory;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeatureIDEParserTest {
+    static FeatureModel featureModel;
+
+    @Test
+    void test() throws FeatureModelParserException {
+        File fileFM = new File("src/test/resources/bamboobike_featureide.xml");
+        FMParserFactory factory = FMParserFactory.getInstance();
+        FeatureModelParser parser = factory.getParser(FMFormat.FEATUREIDE);
+        featureModel = parser.parse(fileFM);
+
+        String expected = """
+                FEATURES:
+                	Bamboo Bike
+                	Frame
+                	Brake
+                	Engine
+                	Drop Handlebar
+                	Female
+                	Male
+                	Step-through
+                	Front
+                	Rear
+                	Back-pedal
+                RELATIONSHIPS:
+                	mandatory(Bamboo Bike, Frame)
+                	mandatory(Bamboo Bike, Brake)
+                	optional(Engine, Bamboo Bike)
+                	optional(Drop Handlebar, Bamboo Bike)
+                	alternative(Frame, Female, Male, Step-through)
+                	or(Brake, Front, Rear, Back-pedal)
+                CONSTRAINTS:
+                	requires(Drop Handlebar, Male)
+                	excludes(Engine, Back-pedal)
+                """;
+
+        assertAll(() -> assertNotNull(featureModel),
+                () -> assertEquals(expected, featureModel.toString()));
+    }
+}
